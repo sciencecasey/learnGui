bb308ae1d079c22be4e770a21e176487eccfa865
Casey Jayne <sciencecaseyj@gmail.com>
2020-05-08 16:29:33 -0700

    Add files via upload

---
+++ celciusconvert/SystemInfo.java
@@ -0,0 +1,13 @@
+package javaclass.celciusconvert;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
